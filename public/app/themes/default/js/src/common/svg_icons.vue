e5fc9ac2a3ac0adff2ae0996194337abbfaf9193
Darryl Hein <dhein@xmmedia.com>
2019-08-14 16:53:05 -0600

    base CSS & JS from other starters

---
+++ public/app/themes/default/js/src/common/svg_icons.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="svg" style="height: 0; width: 0; position: absolute; visibility: hidden;" v-html="svg"></div>
+</template>
+
+<script>
+import fetch from 'unfetch';
+
+export default {
+    props: {
+        src: {
+            type: String,
+            required: true,
+        },
+    },
+
+    data () {
+        return {
+            svg: '',
+        };
+    },
+
+    async mounted () {
+        const response = await fetch(this.src);
+
+        this.svg = await response.text();
+    },
+}
+</script>
+I
---
+++ public/app/themes/default/js/src/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+
+import svg_icons from './common/svg_icons.vue';
+
+// SASS/CSS
+import '../../css/public.scss';
+
+// images
+import '../../images/icons.svg';
+
+// disable the warning about dev/prod
+Vue.config.productionTip = false;
+
+new Vue({
+    el: '#app',
+
+    components: {
+        'svg-icons': svg_icons,
+    },
+});
