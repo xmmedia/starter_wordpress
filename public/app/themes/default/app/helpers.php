0fb8c6752ebfe4b59fd9706fa857c0fe9a53d0fd
Darryl Hein <dhein@xmmedia.com>
2019-08-15 12:16:40 -0600

    cache manifest file & code formatting

--- public/app/themes/default/app/helpers.php
+++ public/app/themes/default/app/helpers.php
@@ -4,21 +4,32 @@ declare(strict_types=1);
 
 class ThemeHelpers
 {
+    /** @var array */
+    public static $manifestData;
+
+    // @todo cache manifest data
     public static function assetPath(string $asset): string
     {
-        $manifestFile = get_template_directory_uri().'/build/manifest.json';
-        $manifestData = \json_decode(\file_get_contents($manifestFile), true);
-        if (0 < \json_last_error()) {
-            throw new \RuntimeException(
-                sprintf(
-                    'Error parsing JSON from asset manifest file "%s" - %s',
-                    $manifestFile,
-                    json_last_error_msg()
-                )
+        if (null === self::$manifestData) {
+            $manifestFile = get_template_directory_uri().'/build/manifest.json';
+
+            self::$manifestData = \json_decode(
+                \file_get_contents($manifestFile),
+                true
             );
+
+            if (0 < \json_last_error()) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Error parsing JSON from asset manifest file "%s" - %s',
+                        $manifestFile,
+                        json_last_error_msg()
+                    )
+                );
+            }
         }
 
-        $path = isset($manifestData[$asset]) ? $manifestData[$asset] : $asset;
+        $path = isset(self::$manifestData[$asset]) ? self::$manifestData[$asset] : $asset;
 
         return get_template_directory_uri().$path;
     }
