bcb48fdf185e07322d2228325ac6ee328b35d1b4
Darryl Hein <dhein@xmmedia.com>
2019-08-14 20:26:03 -0600

    basic theme files

---
+++ public/app/themes/default/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> prefix="og: http://ogp.me/ns#">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+
+    <?php /*<title><?php Common::e($page_title); ?></title>
+
+    <link href="<?php Common::version_file('build/public.css'); ?>" rel="stylesheet">*/ ?>
+</head>
