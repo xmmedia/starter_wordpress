3c4d0285ee455a895566c611c90307def92ece3c
Darryl Hein <dhein@xmmedia.com>
2019-08-15 15:47:20 -0600

    tailwind config tabbing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  theme: {
-    extend: {
-        colors: {
-            'inherit': 'inherit',
-        },
-        borderWidth: {
-            '10': '10px',
-        },
-        maxWidth: {
-            '1/2': '50%',
-            '3/5': '60%',
-            '11/12': '91%',
-        },
-        screens: {
-            'xs': '400px',
-            'max': '1245px',
+    theme: {
+        extend: {
+            colors: {
+                'inherit': 'inherit',
+            },
+            borderWidth: {
+                '10': '10px',
+            },
+            maxWidth: {
+                '1/2': '50%',
+                '3/5': '60%',
+                '11/12': '91%',
+            },
+            screens: {
+                'xs': '400px',
+                'max': '1245px',
+            },
         },
     },
-  },
-  variants: {},
-  plugins: []
-}
+    variants: {},
+    plugins: [],
+};
