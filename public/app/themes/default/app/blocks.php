5d67695fd4ab77477b3519f06d52a133795dbd24
Darryl Hein <dhein@xmmedia.com>
2019-09-17 20:12:14 -0600

    add block category & tweaks

--- public/app/themes/default/app/blocks.php
+++ public/app/themes/default/app/blocks.php
@@ -1,5 +1,26 @@
 <?php
 
+// @todo-wordpress either comment out, delete, or modify for the site's uses
+
+function theme_block_categories($categories, $post)
+{
+    if (!in_array($post->post_type, ['post', 'page'])) {
+        return $categories;
+    }
+
+    return array_merge(
+        $categories,
+        [
+            [
+                'slug'  => 'custom',
+                'title' => __('Custom', 'custom'),
+                'icon'  => 'star-filled',
+            ],
+        ]
+    );
+}
+add_filter('block_categories', 'theme_block_categories', 10, 2);
+
 add_action('init', function ()
 {
     wp_register_script(
--- public/app/themes/default/js/src/blocks.js
+++ public/app/themes/default/js/src/blocks.js
@@ -1,13 +1,13 @@
 const { registerBlockType } = wp.blocks;
 
 registerBlockType('default-theme/example', {
-    title: 'Example: Basic',
-    icon: 'text-page',
-    category: 'layout',
+    title: 'Static Content',
+    icon: 'format-aside',
+    category: 'custom',
     edit () {
-        return <div className="p-5 bg-red-500 text-white">Hello World, (in the editor).</div>;
+        return <div className="p-5 bg-red-800 text-white">Hello World, (in the editor).</div>;
     },
     save () {
-        return <div className="p-5 bg-red-500 text-white">Hello World, (on the page).</div>;
+        return <div className="p-5 bg-red-800 text-white">Hello World, (on the page).</div>;
     },
 });
