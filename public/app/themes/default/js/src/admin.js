a65e79974708e185fd762fd85d4f1fea70bf8dc7
Darryl Hein <dhein@xmmedia.com>
2019-11-22 11:21:10 -0700

    change var to let

--- public/app/themes/default/js/src/admin.js
+++ public/app/themes/default/js/src/admin.js
@@ -2,13 +2,13 @@
 // from https:// www.sitepoint.com/wordpress-svg/ plus modifications & fixes
 new MutationObserver(function (mutations) {
     //  look through all mutations that just occurred
-    for (var i = 0; i < mutations.length; i++) {
+    for (let i = 0; i < mutations.length; i++) {
 
         //  look through all added nodes of this mutation
-        for (var j = 0; j < mutations[i].addedNodes.length; j++) {
+        for (let j = 0; j < mutations[i].addedNodes.length; j++) {
 
             // get the applicable element
-            var element = $(mutations[i].addedNodes[j]);
+            let element = $(mutations[i].addedNodes[j]);
 
             // execute only if we have a class
             if (element.attr('class')) {
@@ -17,14 +17,14 @@ new MutationObserver(function (mutations) {
                 if (element.attr('class').indexOf('attachment') != -1) {
 
                     // find attachment inner (which contains subtype info)
-                    var attachmentPreview = element.children('.attachment-preview');
+                    let attachmentPreview = element.children('.attachment-preview');
                     if (attachmentPreview.length != 0) {
 
                         // only run for SVG elements
                         if (attachmentPreview.attr('class').indexOf('subtype-svg+xml') != -1) {
 
                             // bind an inner function to element so we have access to it.
-                            var handler = function (element) {
+                            let handler = function (element) {
                                 // do an AJAX call to get the URL
                                 $.ajax({
                                     url: ajaxurl,
