b074bdb9187a36f6ab956dc89799bc47d861422a
Herman Steuernagel <herman@thefourth.ca>
2020-02-21 15:13:53 -0800

    updating variants and font headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,23 @@ module.exports = {
                 'print': { 'raw': 'print' },
                 'retina': { 'raw': '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)' },
             },
+            fontFamily: {
+                'headings': [
+                    '"Helvetica Neue"',
+                    'Arial',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
         },
     },
-    variants: {},
+    variants: {
+        textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        borderColor: ['responsive', 'hover', 'focus', 'group-hover'],
+        opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+    },
     plugins: [],
 };
