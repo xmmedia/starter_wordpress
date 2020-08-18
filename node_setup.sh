bad44f6d350c2d5a7b26f6f3d8ffbf307a62b80f
Darryl Hein <dhein@xmmedia.com>
2020-08-18 14:23:59 -0600

    update nvm & use frozen lock file on node setup

--- node_setup.sh
+++ node_setup.sh
@@ -2,11 +2,13 @@
 
 echo "-- Setting up node"
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
 nvm install
 nvm use --delete-prefix --silent
 
 # install local node packages with yarn
-yarn
+yarn install --frozen-lockfile
 
 # as this is run on the server, we don't want to keep Cypress
 echo "-- Remove Cypress"
