df7bad54a640e456f00f8534e7cf4bab8d2c0dc0
Darryl Hein <dhein@xmmedia.com>
2020-08-18 15:40:49 -0600

    remove ie9 warning

--- public/app/themes/default/header.php
+++ public/app/themes/default/header.php
@@ -10,17 +10,6 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-    <!--[if lte IE 9]>
-    <div class="bg-red-500">
-        <div class="max-w-lg my-0 mx-auto p-3 text-white text-center">
-            <strong class="text-xl">This site does not support the browser you are using.</strong><br>
-            We recommend upgrading your browser as it's out-of-date.<br>
-            Please check <a href="https://browsehappy.com/" class="text-inherit hover:text-grey-600">these instructions regarding updating your browser</a>.<br>
-            We only support evergreen browsers, such as Chrome, Safari, Edge, and Firefox.
-        </div>
-    </div>
-    <![endif]-->
-
     <div id="app">
         <header class="w-content">
             <!-- @todo-wordpress -->
