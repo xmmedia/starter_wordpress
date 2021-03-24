29fd8252c2f8c06bfec8cc89ecbcac4482846af3
Darryl Hein <dhein@xmmedia.com>
2021-03-23 21:53:10 -0600

    keep logs for wordfence to be kept across deploys

--- setup_prod.sh
+++ setup_prod.sh
@@ -67,11 +67,14 @@ fi
 echo "Creating dirs in ${BASE}"
 cd $BASE || exit
 mkdir -p $RELEASE/public/app/uploads
+mkdir -p $RELEASE/public/app/wflogs
 mkdir -p $SHARED/public/app/uploads
+mkdir -p $SHARED/public/app/wflogs
 ln -s $RELEASE current
 rm -rf html
 ln -s current/public html
 ln -s $SHARED/public/app/uploads $RELEASE/public/app/uploads
+ln -s $SHARED/public/app/wflogs $RELEASE/public/app/wflogs
 printf "\n\n"
 
 cd $RELEASE || exit
