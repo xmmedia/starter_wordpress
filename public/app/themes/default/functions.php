3001951e7c5c5d825c06cef1d105e83854ac85b3
Herman Steuernagel <herman@thefourth.ca>
2022-08-15 11:56:22 -0700

    add support for woo gallery

--- public/app/themes/default/functions.php
+++ public/app/themes/default/functions.php
@@ -134,6 +134,9 @@ add_action('after_setup_theme', function () {
 
     // Allow Support for WooCommerce
     add_theme_support('woocommerce');
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
 
     /**
      * Add additional image sizes
