cd6c0568472edb366d613249e67a853afd3d9417
Darryl Hein <dhein@xmmedia.com>
2023-01-31 11:40:58 -0700

    add new code for google analytics

--- public/app/themes/default/header.php
+++ public/app/themes/default/header.php
@@ -5,6 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php wp_head(); ?>
+
+    <?php // @todo-wordpress add favicon tags here ?>
+
+    <?php if (Env\env('GA_ANALYTICS_ID')) : ?>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo Env\env('GA_ANALYTICS_ID'); ?>"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '<?php echo Env\env('GA_ANALYTICS_ID'); ?>');
+        </script>
+    <?php endif; ?>
 </head>
 
 <body <?php body_class(); ?>>
