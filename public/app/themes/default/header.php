2bd86227021cfd5033c28b4094cba8b8dc7e9866
Megan Sych <msych@xmmedia.com>
2025-12-09 08:35:06 -0500

    update favicons

--- public/app/themes/default/header.php
+++ public/app/themes/default/header.php
@@ -6,7 +6,12 @@
 
     <?php wp_head(); ?>
 
-    <?php // @todo-wordpress add favicon tags here ?>
+    <?php // @todo-wordpress update favicon tags here ?>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="XM Media" />
 
     <?php if (Env\env('GA_ANALYTICS_ID')) : ?>
         <!-- Google tag (gtag.js) -->
